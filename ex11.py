787197057f50b9f3b29db51854edacec092618d1
Avadhoot Kulkarni <avadhoot221266@gmail.com>
2015-05-21 09:51:29 +0530

    Print it!
    argv & raw_input()

---
+++ ex11.py
@@ -0,0 +1,16 @@
+print "How old are you?",
+age = input()
+
+print "How tall are you (in cms)?",
+height = input()
+
+print "How much do you weigh?",
+weight = input()
+
+print "So, you're %d years old, %d cms tall, and %d kg" % (
+age, height, weight)
+
+print "Where do you work?",
+place = raw_input()
+
+print "%r is an awesome placce to work" % place
---
+++ ex13.py
@@ -0,0 +1,18 @@
+from sys import argv
+
+##print "First fruit: "
+#first = raw_input()
+
+#print "Second fruit: "
+#second = raw_input()
+
+#print "Third fruit: "
+#third = raw_input()
+
+script, first, second, third = argv
+
+
+print "This is the: ", script
+print "The first variable is: ", first
+print "The second variable is: ", second
+print "The third variable is: ", third
